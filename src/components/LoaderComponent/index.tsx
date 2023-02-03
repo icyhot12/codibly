1973156fcf57b268feb10791e6221f30a760f115
Rafał Mierniczek <rmierniczek1@gmail.com>
2023-02-03 11:53:16 +0100

    add alt image

--- src/components/LoaderComponent/index.tsx
+++ src/components/LoaderComponent/index.tsx
@@ -1,7 +1,7 @@
 const LoaderComponent = () => {
   return (
     <div className="mx-auto mt-32">
-      <img src="/images/spinner.svg" />
+      <img src="/images/spinner.svg" alt="loader"/>
     </div>
   );
 };
